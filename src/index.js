e1239a4e46d7d84539c93cb9f66e2d30b8da5a90
Mohammad Hematy <hematy61@gmail.com>
2019-05-29 23:35:25 -0500

    set up public directory and serve home route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 const port = process.env.PORT
+
+// define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+
+/*******************************************************************
+ ***************** home route **************************************
+ *******************************************************************/
+app.use(express.static(publicDirectoryPath))
+app.get('', (req, res) => {
+  res.send('<h1>Welcome to Chat app with Socket.io</h1>')
+})
 
 
 /*******************************************************************
